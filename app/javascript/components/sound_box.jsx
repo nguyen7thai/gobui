1e7272a74c6997f4f7440abb654ae94778de5c49
Thai Nguyen <nguyen7thai@gmail.com>
2017-04-18 09:33:04 +0700

    Move to React: Sound box

--- app/javascript/components/sound_box.jsx
+++ app/javascript/components/sound_box.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
 import ReactDom from 'react-dom'
+import Dispatcher from '../dispatcher'
 
 export default class SoundBox extends React.Component {
+  constructor() {
+    super()
+    Dispatcher.subscribe('play_sound', (data) => {
+      let audio = new Audio(`/assets/${data.file_name}`)
+      audio.play()
+    })
+  }
+  triggerSound(fileName) {
+    return () => {
+      window.App.chatChannel.send({type: 'play_sound', file_name: fileName})
+    }
+  }
   render() {
-    return <div id='sound-box'> Sound Box Here
+    return <div id='sound-box'>
+      <a href='#' onClick={this.triggerSound('buzz.mp3')} >Buzz</a>
+      <a href='#' onClick={this.triggerSound('bebe_voice.mp3')} >Go Bùi</a>
+      <a href='#' onClick={this.triggerSound('thapbui_voice.wav')} >Go Tháp Bùi</a>
     </div>
   }
 }
